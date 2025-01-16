9667dbf2dbad985bc64278184a949997b1309bb2
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-16 23:58:56 +0300

    add toastify container

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
+}
+
+export default MyApp;
